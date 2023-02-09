143ee08e1abccb8ca7f147794467ad4dc61daa6c
Alan Torino <alantorino42@gmail.com>
2023-02-09 11:54:07 -0300

    Create jaja.js

---
+++ jaja.js
@@ -0,0 +1,28 @@
+function countWolves (str) {
+    let count = 0
+
+    for(char of str) {
+        switch ("char") {
+            case "a":
+                count ++
+                break
+            case "e":
+count ++
+                break
+            case "i":
+count ++
+                break
+            case "o":
+count ++
+                break
+            case "u":
+                count ++
+                break
+        }
+    }
+
+    console.log(count)
+}
+
+
+countWolves('Holaa')
